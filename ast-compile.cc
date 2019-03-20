f38ba9db1f43271e43348981e44527bd70d2e955
KOLLU SATHVIK REDDY <sathvik@cse.iitb.ac.in>
2019-03-20 15:40:33 +0530

    ast-compile-start

---
+++ ast-compile.cc
@@ -0,0 +1,141 @@
+#include "ast.hh"
+template class Number_Ast<double>;
+template class Number_Ast<int>;
+
+
+Code_For_Ast & Ast::create_store_stmt(Register_Descriptor * store_register)
+{
+
+}
+
+Code_For_Ast & Assignment_Ast::compile()
+{
+
+}
+Code_For_Ast & Assignment_Ast::compile_and_optimize_ast(Lra_Outcome & lra)
+{
+
+}
+
+Code_For_Ast & Name_Ast::compile()
+{
+
+}
+Code_For_Ast & Name_Ast::compile_and_optimize_ast(Lra_Outcome & lra)
+{
+
+}
+Code_For_Ast & Name_Ast::create_store_stmt(Register_Descriptor * store_register)
+{
+
+}
+
+
+template <class T>
+Code_For_Ast & Number_Ast<T>::compile()
+{
+
+}
+Code_For_Ast & Number_Ast<T>::compile_and_optimize_ast(Lra_Outcome & lra)
+{
+
+}
+
+Code_For_Ast & Plus_Ast::compile()
+{
+
+}
+Code_For_Ast & Plus_Ast::compile_and_optimize_ast(Lra_Outcome & lra)
+{
+
+}
+
+Code_For_Ast & Minus_Ast::compile()
+{
+
+}
+Code_For_Ast & Minus_Ast::compile_and_optimize_ast(Lra_Outcome & lra)
+{
+
+}
+
+Code_For_Ast &  Divide_Ast::compile()
+{
+
+}
+Code_For_Ast &  Divide_Ast::compile_and_optimize_ast(Lra_Outcome & lra)
+{
+
+}
+
+Code_For_Ast & Mult_Ast::compile()
+{
+
+}
+Code_For_Ast & Mult_Ast::compile_and_optimize_ast(Lra_Outcome & lra)
+{
+
+}
+
+Code_For_Ast & UMinus_Ast::compile()
+{
+
+}
+Code_For_Ast & UMinus_Ast::compile_and_optimize_ast(Lra_Outcome & lra)
+{
+
+}
+
+Code_For_Ast & Conditional_Expression_Ast::compile()
+{
+
+}
+// Code_For_Ast & Conditional_Expression_Ast::compile_and_optimize_ast(Lra_Outcome & lra);
+
+Code_For_Ast & Return_Ast::compile()
+{
+
+}
+Code_For_Ast & Return_Ast::compile_and_optimize_ast(Lra_Outcome & lra)
+{
+
+}
+
+Code_For_Ast & Relational_Expr_Ast::compile()
+{
+
+}
+// Code_For_Ast & Relational_Expr_Ast::compile_and_optimize_ast(Lra_Outcome & lra);
+
+Code_For_Ast & Logical_Expr_Ast::compile()
+{
+
+}
+// Code_For_Ast & Logical_Expr_Ast::compile_and_optimize_ast(Lra_Outcome & lra);
+
+Code_For_Ast & Selection_Statement_Ast::compile()
+{
+
+}
+// Code_For_Ast & Selection_Statement_Ast::compile_and_optimize_ast(Lra_Outcome & lra);
+
+Code_For_Ast & Iteration_Statement_Ast::compile()
+{
+
+}
+// Code_For_Ast & Iteration_Statement_Ast::compile_and_optimize_ast(Lra_Outcome & lra);
+
+Code_For_Ast & Sequence_Ast::compile()
+{
+
+}
+void Sequence_Ast::print_assembly(ostream & file_buffer)
+{
+
+}
+void Sequence_Ast::print_icode(ostream & file_buffer)
+{
+
+}
+
+
